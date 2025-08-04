e787b4b8ff524a45211ec277217464d4f9dc00dd
zamkid <minecrafttvem@gmail.com>
2025-08-04 02:28:36 +0200

    added local file only for developer needed to .gitignore

--- Auth_service/src/database/pg/async_session.py
+++ Auth_service/src/database/pg/async_session.py
@@ -5,7 +5,7 @@ from src.config.settings import settings
 DATABASE_URL = settings.DATABASE_URL
 
 engine = create_async_engine(DATABASE_URL, echo=True)
-SessionLocal = async_sessionmaker(bind=engine, class_=AsyncSession, expire_on_commit=False)
+SessionLocal = async_sessionmaker(bind=engine, class_=AsyncSession, expire_on_commit=True)
 
 async def get_db():
     async with SessionLocal() as session:
