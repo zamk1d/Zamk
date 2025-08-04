a0fa84f2de77df8b127e996b4aaf5cd36db193fe
zamkid <minecrafttvem@gmail.com>
2025-08-04 02:38:02 +0200

    logs turned off

--- Auth_service/src/database/pg/async_session.py
+++ Auth_service/src/database/pg/async_session.py
@@ -4,7 +4,7 @@ from src.config.settings import settings
 
 DATABASE_URL = settings.DATABASE_URL
 
-engine = create_async_engine(DATABASE_URL, echo=True)
+engine = create_async_engine(DATABASE_URL, echo=False)
 SessionLocal = async_sessionmaker(bind=engine, class_=AsyncSession, expire_on_commit=True)
 
 async def get_db():
