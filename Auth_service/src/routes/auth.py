b076a9256c46fc44656e521f5786fe42d124b54a
zamkid <minecrafttvem@gmail.com>
2025-08-04 02:40:11 +0200

    added business logic to each endpoint

--- Auth_service/src/routes/auth.py
+++ Auth_service/src/routes/auth.py
@@ -8,6 +8,7 @@ from src.config.cookie_settings import RefreshCookieSettings
 from src.database.pg.async_session import get_db
 from src.schemas.routes.auth.auth_schemas import LoginSchema, RegisterSchema
 from src.schemas.routes.token.token_schemas import TokenResponseSchema, AccessTokenSchema
+from src.services.auth import register, login
 
 auth = APIRouter(prefix="/auth", tags=["auth"])
 
@@ -19,29 +20,24 @@ async def login_user(
         response: Response,
         db: AsyncSession = Depends(get_db)
     ) -> AccessTokenSchema:
-
     """Validates response data and returns access + refresh tokens if data is valid"""
 
-    # db method
-
-    tokens = TokenResponseSchema(access_token="123", refresh_token="456")
+    tokens = await login(**data.model_dump(), db=db)
     response.set_cookie(
         value=tokens.refresh_token,
         **RefreshCookieSettings().model_dump()
     )
     return AccessTokenSchema(access_token=tokens.access_token)
 
-@auth.post("/register", response_model=AccessTokenSchema)
+@auth.post("/register", response_model=AccessTokenSchema, status_code=201)
 async def register_user(
         data: Annotated[RegisterSchema, Body()],
         response: Response,
         db: AsyncSession = Depends(get_db)
-    ) -> AccessTokenSchema:
+) -> AccessTokenSchema:
     """Add new user if not exists to clients database"""
 
-    # db method
-
-    tokens = TokenResponseSchema(access_token="123", refresh_token="456")
+    tokens = await register(**data.model_dump(), db=db)
     response.set_cookie(
         value=tokens.refresh_token,
         **RefreshCookieSettings().model_dump()
--- Auth_service/src/routes/token.py
+++ Auth_service/src/routes/token.py
@@ -3,17 +3,29 @@ from fastapi import APIRouter, Depends, Cookie, Response
 from sqlalchemy.ext.asyncio import AsyncSession
 from typing import Annotated
 
+from src.config.cookie_settings import RefreshCookieSettings
 from src.database.pg.async_session import get_db
 from fastapi.security import OAuth2PasswordBearer
 
 from src.schemas.routes.token.token_schemas import AccessTokenSchema
+from src.services.security.token_signature import verify_token
+from src.services.token import refresh
 
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/token")
 
 token = APIRouter(prefix="/tokens", tags=["tokens"])
 
 @token.get("/refresh", response_model=AccessTokenSchema)
-async def refresh_tokens(refresh_token: Annotated[str, Cookie()], response: Response, db: AsyncSession = Depends(get_db)) -> AccessTokenSchema:
+async def refresh_tokens(
+        refresh_token: Annotated[str, Cookie()],
+        response: Response,
+        db: AsyncSession = Depends(get_db)
+) -> AccessTokenSchema:
     """Validates user refresh token from cookie and return access + refresh tokens"""
 
-    return AccessTokenSchema(access_token=refresh_token)
+    tokens = await refresh(refresh_token=refresh_token, db=db)
+    response.set_cookie(
+        value=tokens.refresh_token,
+        **RefreshCookieSettings().model_dump()
+    )
+    return AccessTokenSchema(access_token=tokens.access_token)
