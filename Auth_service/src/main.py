e867c92b1dec27b19595f429c970d1c35e62dbfa
zamkid <minecrafttvem@gmail.com>
2025-08-04 06:26:14 +0200

    added rabbit queue listener

--- Auth_service/src/main.py
+++ Auth_service/src/main.py
@@ -4,16 +4,15 @@ from contextlib import asynccontextmanager
 from fastapi import FastAPI
 from src.routes.auth import auth
 from src.routes.token import token
-
-from src.database.pg.init_db import init_db
+from src.database.pg.drop_init_func import init_db
+from src.rabbitMQ.rabbit_handler import consume_messages
 
 @asynccontextmanager
 async def startup_event(app: FastAPI):
-    await init_db()
+    # await init_db()
+    asyncio.create_task(consume_messages(queue_name="auth_queue"))
     yield
 
-app = FastAPI()
+app = FastAPI(lifespan=startup_event)
 app.include_router(auth)
 app.include_router(token)
-
-
