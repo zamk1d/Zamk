5709374d98d08b962236ff86e726a89fe05c3f63
zamkid <minecrafttvem@gmail.com>
2025-09-29 22:19:58 +0200

    added empty default values to db urls to solve empty parameters error

--- Auth_service/src/config/settings.py
+++ Auth_service/src/config/settings.py
@@ -1,14 +1,13 @@
 """Env settings."""
-from pydantic_settings import BaseSettings
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
 
 class Settings(BaseSettings):
-    DATABASE_URL: str
-    RABBIT_URL: str
+    DATABASE_URL: str = ""
+    RABBIT_URL: str = ""
     att: int = 15
     rtt: int = 7
 
-    class Config:
-        env_file = ".env"
-        env_file_encoding = 'utf-8'
+    model_config = SettingsConfigDict(env_file=".env", env_file_encoding="utf-8")
 
 settings = Settings()
